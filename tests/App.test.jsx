2fa46711857b87edec9c45ab2df4f2a6259a0cd0
futurebound <mitchellm1618@gmail.com>
2024-08-07 18:46:10 -0700

    test: basic h1 rendering test passing

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -2,22 +2,10 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
-
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
-});
-
 describe('App', () => {
   it('renders headline', () => {
-    render(<App title='React' />);
-
-    screen.debug();
-
     // check if App components renders headline
+    render(<App />);
+    expect(screen.getByRole('heading').textContent).toMatch(/our first test/i);
   });
 });

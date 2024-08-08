677f6168db4b0005c78dcf494e7109264715fedc
futurebound <mitchellm1618@gmail.com>
2024-08-07 18:50:32 -0700

    test: added dynamic button render and mocked clicks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
+
   return (
     <>
-      <h1>Our First Test</h1>
+      <button type='button' onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
     </>
   );
 }
--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,11 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../src/App';
 
 describe('App', () => {
-  it('renders headline', () => {
-    // check if App components renders headline
+  it('renders magnificent monkeys', () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders radical rhinos after button click', async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByRole('heading').textContent).toMatch(/our first test/i);
+    const button = screen.getByRole('button', { name: 'Click Me' });
+
+    await user.click(button);
+
+    expect(screen.getByRole('heading').textContent).toMatch(/radical rhinos/i);
   });
 });
